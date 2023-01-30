af80cfdcc612d0dbe08e09c4a3fc798a3b35ad3c
Alferavil88 <alferavil88@gmail.com>
2023-01-30 17:17:44 -0500

    feat: Add page not found component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,15 @@
 import "./Navbar.scss";
+import Logo from "../../assets/svg/logo-no-background.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="dietify" />
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
